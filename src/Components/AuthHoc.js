f174707e1b246b09cd50f6a943c8aa7dbc9296ad
dekimage <dejan.gavrilovikk@gmail.com>
2024-12-23 16:00:41 +0100

    aaa

--- src/Components/AuthHoc.js
+++ src/Components/AuthHoc.js
@@ -10,35 +10,35 @@ import { toJS } from "mobx";
 
 const withAuth = (WrappedComponent) => {
   const AuthComponent = observer((props) => {
-    const router = useRouter();
-    const { user, loading } = MobxStore;
-    const [verified, setVerified] = useState(false);
-    console.log(11, toJS(user));
+    // const router = useRouter();
+    // const { user, loading } = MobxStore;
+    // const [verified, setVerified] = useState(false);
+    // console.log(11, toJS(user));
 
-    useEffect(() => {
-      const checkEmailVerification = async () => {
-        console.log(user);
-        if (user) {
-          // Fetch emailVerified status from Firestore
-          // const userDoc = await db.collection("users").doc(user.uid).get();
-          // const userData = userDoc.data();
+    // useEffect(() => {
+    //   const checkEmailVerification = async () => {
+    //     console.log(user);
+    //     if (user) {
+    //       // Fetch emailVerified status from Firestore
+    //       // const userDoc = await db.collection("users").doc(user.uid).get();
+    //       // const userData = userDoc.data();
 
-          if (user && user.emailVerified) {
-            setVerified(true);
-          } else {
-            router.push("/verify-email");
-          }
-        }
-      };
+    //       if (user && user.emailVerified) {
+    //         setVerified(true);
+    //       } else {
+    //         router.push("/verify-email");
+    //       }
+    //     }
+    //   };
 
-      if (!loading) {
-        checkEmailVerification();
-      }
-    }, [loading, user, router]);
+    //   if (!loading) {
+    //     checkEmailVerification();
+    //   }
+    // }, [loading, user, router]);
 
-    if (loading || !verified) {
-      return <Loader />;
-    }
+    // if (loading || !verified) {
+    //   return <Loader />;
+    // }
 
     return <WrappedComponent {...props} />;
   });
